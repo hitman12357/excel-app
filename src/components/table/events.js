f276b1f73f1655495f89af394bb1dc5aabad860e
Andrey <hitman12357@gmail.com>
2021-02-25 18:46:25 +0300

    resize columns, rows

---
+++ src/components/table/events.js
@@ -0,0 +1,42 @@
+export const resize = ($root, $resizer) => {
+  const $parent = $resizer.closest('[data-type="resizable"]')
+  const coords = $parent.getCoords()
+  const type = $resizer.data.resize
+
+  const sideProp = type === 'col' ? 'bottom' : 'right'
+  const coordsProp = type === 'col' ? 'right' : 'bottom'
+  const coordsDimension = type === 'col' ? 'pageX' : 'pageY'
+  const lengthProp = type === 'col' ? 'width' : 'height'
+
+  let value
+
+  $resizer.css({
+    opacity: 1,
+    [sideProp]: '-5000px'
+  })
+
+  document.onmousemove = e => {
+    const delta = e[coordsDimension] - coords[coordsProp]
+    value = coords[lengthProp] + delta
+    $resizer.css({[coordsProp]: -delta + 'px'})
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null
+    document.onmouseup = null
+
+    $parent.css({[lengthProp]: value + 'px'})
+
+    if (type === 'col') {
+      $root
+          .findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => el.style.width = value + 'px')
+    }
+
+    $resizer.css({
+      opacity: 0,
+      bottom: 0,
+      right: 0
+    })
+  }
+}
